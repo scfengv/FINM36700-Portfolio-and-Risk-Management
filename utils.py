1331359d7d4a175ee83bf4a1a4d3d41002af6f73
scfengv <scvfeng27@gmail.com>
2025-10-17 21:36:19 -0500

    Update HW3

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 def CalcVar(returns: pd.Series):
@@ -6,3 +7,27 @@ def CalcVar(returns: pd.Series):
 def CalcCVar(returns: pd.Series):
     return returns[returns <= CalcVar(returns)].mean()
 
+def CalcLevelReturns(returns: pd.Series, annualizedFactor: int) -> float:
+    """_summary_
+    Args:
+        returns (pd.Series): Times Series returns data (monthly / weekly / daily)
+            - Date as index
+        annualizedFactor (int): monthly = 12; weekly = 52; daily = 252
+        
+    Returns:
+        Annualized mean of Level Return
+    """
+    return returns.mean() * annualizedFactor
+
+def CalcLogReturns(returns: pd.Series, annualizedFactor: int) -> float:
+    """_summary_
+
+    Args:
+        returns (pd.Series): Times Series returns data (monthly / weekly / daily)
+        annualizedFactor (int): monthly = 12; weekly = 52; daily = 252
+
+    Returns:
+        Annualized mean of Log Return
+    """
+    logReturns = np.log1p(1 + returns)
+    return logReturns.mean() * annualizedFactor
